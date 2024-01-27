c5217ff409ac78bbb3b5f72912554d0c0c06fe55
Ali Hefny <ahefny@ejada.com>
2024-01-27 20:22:20 +0200

    base urls added

--- src/services/envConstants.ts
+++ src/services/envConstants.ts
@@ -1,1 +1,3 @@
-export const apis = {};
+const TransactionURL = 'https://fakestoreapi.com/products?limit=5';
+const ExpensesURL = 'https://api.escuelajs.co/api/v1/products';
+export const apis = {TransactionURL, ExpensesURL};

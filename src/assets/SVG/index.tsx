597569affe05f485eb65be272346e4ae01899ace
Ali Hefny <ahefny@ejada.com>
2024-01-27 20:22:59 +0200

    modify svgs and icons to be used

---
+++ src/assets/SVG/index.tsx
@@ -0,0 +1,5 @@
+export {default as Home} from './Home';
+export {default as Retail} from './Retail';
+export {default as Profile} from './Profile';
+export {default as Favorite} from './Favorite';
+export {default as Note} from './Note';
